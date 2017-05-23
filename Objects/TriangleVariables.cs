b15df70f1d270dad71cbf505027c5e96b0c87b9e
david rolfs and ethan luts <david+ethan@>
2017-05-22 17:42:25 -0700

    add working branch

--- Objects/TriangleVariables.cs
+++ Objects/TriangleVariables.cs
@@ -6,6 +6,7 @@ namespace Triangle.Objects
     private int _side2;
     private int _side3;
 
+
     public int GetSide1()
     {
       return _side1;
@@ -31,31 +32,16 @@ namespace Triangle.Objects
       _side3 = newSide3;
     }
 
-
     public int GetTriangleType()
     {
-      return side1_;
-      // if(_side1 + _side2 <= _side3 || _side2 + _side3 <= _side1 || _side1 + _side3 <= _side2)
-      // {
-      //   return (_side1);
-      // }
-      // else
-      // {
-      //   if(_side1 == _side2 && _side1 == _side3)
-      //   {
-      //     //Equilateral
-      //     return (_side1);
-      //   }
-      //   else if(_side1 == _side2 || _side1 == _side3 || _side2 == _side3)
-      //   {
-      //     //Isosceles
-      //     return (_side2);
-      //   }
-      //   else if(_side1 !=_side2 && _side1 != _side3 && _side2 != _side3)
-      //   {
-      //     //Scalene
-      //     return (_side3);
-      //   }
+      if(_side1 == _side2)
+      {
+        return _side1;
+      }
+      else
+      {
+        return _side2;
       }
     }
   }
+}
